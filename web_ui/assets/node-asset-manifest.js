3ec852feb78ca1e87da1a69f16aa9c8c4d35cefd
catsby <clint@ctshryock.com>
2021-03-24 15:44:18 -0500

    copy over the webui

---
+++ web_ui/assets/node-asset-manifest.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+define('vault/config/node-asset-manifest', function() {
+  return {
+    default: {"bundles":{"kmip":{"assets":[{"uri":"/ui/engines-dist/kmip/assets/engine-104be8b53671766e11f4c0eec49518d0.js","type":"js"},{"uri":"/ui/engines-dist/kmip/config/environment-0123205ae026fc9ed3e41f1d552270f8.js","type":"js"}]},"open-api-explorer":{"assets":[{"uri":"/ui/engines-dist/open-api-explorer/assets/engine-62ae6a1404951ee9b6a34d6fc100eb08.js","type":"js"},{"uri":"/ui/engines-dist/open-api-explorer/assets/engine-9dcfdf942f31c3caa1d6dfd57c3cc072.css","type":"css"},{"uri":"/ui/engines-dist/open-api-explorer/assets/engine-vendor-700b2133eae35e9a3196b857dad68111.css","type":"css"},{"uri":"/ui/engines-dist/open-api-explorer/config/environment-6da0fcce17b2031e2559754701e92d69.js","type":"js"}]},"replication":{"assets":[{"uri":"/ui/engines-dist/replication/assets/engine-c6200f70b0f831975f7e679e9c1c1d34.js","type":"js"},{"uri":"/ui/engines-dist/replication/config/environment-fcc3a0f22bdfd265a50708864776440a.js","type":"js"}]}}}
+  };
+});
